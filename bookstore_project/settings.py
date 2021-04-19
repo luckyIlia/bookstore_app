f47e2d7e09cf92bf96321090116bae37fdd65892
luckyilia <ilia.dev@yahoo.com>
2021-04-19 16:21:27 +1200

    'ch10'

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # Local
     'pages.apps.PagesConfig',
     'accounts.apps.UsersConfig',
+    'books.apps.BooksConfig',
 ]
 
 MIDDLEWARE = [
@@ -168,3 +169,4 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
