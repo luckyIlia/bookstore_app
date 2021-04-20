308d0437d94599b7cb024b328d5189b595d78c79
luckyilia <ilia.dev@yahoo.com>
2021-04-20 23:02:15 +1200

    added docker-compose-prod

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -15,7 +15,7 @@ import os
 import socket
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
