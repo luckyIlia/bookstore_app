ac91e71a79365714ca8bebcee60509f665b154f9
luckyilia <ilia.dev@yahoo.com>
2021-04-20 23:51:39 +1200

    'ch17'

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
                   # Django admin
-                  path('admin/', admin.site.urls),
+                  path('boom-beep/', admin.site.urls),
 
                   # User management
                   path('accounts/', include('allauth.urls')),
@@ -32,8 +32,4 @@ urlpatterns = [
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-                      path('__debug__/', include(debug_toolbar.urls))
-                  ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
